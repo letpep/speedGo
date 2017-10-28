522d6d2ca46c85863ddd436fc3ed76835c9b00ef
李信 <li.xin@sf-express.com>
2017-10-28 17:44:27 +0800

    table使用

---
+++ luafiles/tabledemo.lua
@@ -0,0 +1,11 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: lee_xin
+-- Date: 17/10/28
+-- Time: 下午5:39
+-- To change this template use File | Settings | File Templates.
+--
+ local   json = require(resty.cjson)
+  local  tablea = {key1="value1",key2=3 }
+    local jsonstr = json.encode(tablea)
+    ngx.say(jsonstr)
