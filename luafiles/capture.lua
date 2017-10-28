b70dc5c0179bdd680a04d7bed58dcdb7a304513f
李信 <li.xin@sf-express.com>
2017-10-28 13:36:07 +0800

    capture_multi 测试样例

---
+++ luafiles/capture.lua
@@ -0,0 +1,18 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: lee_xin
+-- Date: 17/10/28
+-- Time: 下午1:27
+-- To change this template use File | Settings | File Templates.
+--
+local res1,res2 = ngx.location.capture_multi{
+    {"/redis_get_set", {args="key=lixin"}},
+    {"/ordersuc"}
+}
+
+ngx.header.content_type="text/plain"
+ngx.say(res1.body)
+ngx.say(res2.body)
+
+ngx.say(res2.status)
+ngx.say(res2.header["Set-Cookie"])
