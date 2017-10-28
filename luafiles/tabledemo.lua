553fae7a05de2534c08d9cccf93e0802f6e4bfee
李信 <li.xin@sf-express.com>
2017-10-28 17:59:33 +0800

    table使用3

--- luafiles/tabledemo.lua
+++ luafiles/tabledemo.lua
@@ -5,6 +5,13 @@
 -- Time: 下午5:39
 -- To change this template use File | Settings | File Templates.
 --
+local function t2str(str)
+    local res = ''
+    for k,v in ipairs (str) do
+    res= res..k..v
+    end
+    return res
+end
     local json = require("cjson")
     local  tablea = {key1="value1",key2=3 }
     local jsonstr = json.encode(tablea)
@@ -15,4 +22,8 @@
     t1['key2'] = 2
     jsonstr = json.encode(t1)
     ngx.say(jsonstr)
---{"key2":3,"key1":"value1"}
+--{"key2":2,"key1":"value1"}
+    local json2 = '{"key2":2,"key1":"value1"}'
+    local tt2 = json.decode(json2)
+    jsonstr = t2str(json2)
+    ngx.say(jsonstr)
