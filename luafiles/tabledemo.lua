4d64b44ae47b3bef3df13cd8830382c4982ef0b2
李信 <li.xin@sf-express.com>
2017-10-28 17:52:00 +0800

    table使用

--- luafiles/tabledemo.lua
+++ luafiles/tabledemo.lua
@@ -6,6 +6,12 @@
 -- To change this template use File | Settings | File Templates.
 --
     local json = require("cjson")
-  local  tablea = {key1="value1",key2=3 }
+    local  tablea = {key1="value1",key2=3 }
     local jsonstr = json.encode(tablea)
+    --ngx.say(jsonstr)
+--{"key2":3,"key1":"value1"}
+    local t1 = {}
+    t1["key1"] = 'value1'
+    t1['key2'] = 2
+    jsonstr = json.encode(tablea)
     ngx.say(jsonstr)
