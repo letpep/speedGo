fab35a1b60c9c3e76fa191c283cfde44a83762f9
李信 <li.xin@sf-express.com>
2017-12-15 16:58:36 +0800

    修改url

--- luafiles/curl_http_demo.lua
+++ luafiles/curl_http_demo.lua
@@ -6,7 +6,8 @@
 -- To change this template use File | Settings | File Templates.
 --
 
-local handle =io.popen("curl http://baidu.com")
+
+local handle =io.popen('curl -l -H "kbn-version:5.6.4" -X POST "http://10.102.4.254:5601/api/console/proxy?path=_search&method=POST" -d '..'{"version":true,"size":0,"sort":[{"@timestamp":{"order":"desc","unmapped_type":"boolean"}}],"query":{"bool":{"must":[{"query_string":{"analyze_wildcard":true,"query":"\"timed out\""}},{"match_phrase":{"path":{"query":"*errInvokerResult*"}}},{"range":{"@timestamp":{"gte":1513267200000,"lte":1513353599999,"format":"epoch_millis"}}}],"must_not":[]}},"_source":{"excludes":[]},"aggs":{"2":{"date_histogram":{"field":"@timestamp","interval":"30m","time_zone":"Asia/Shanghai","min_doc_count":1}}},"stored_fields":["*"],"script_fields":{},"docvalue_fields":["@timestamp"],"highlight":{"pre_tags":["@kibana-highlighted-field@"],"post_tags":["@/kibana-highlighted-field@"],"fields":{"*":{"highlight_query":{"bool":{"must":[{"query_string":{"analyze_wildcard":true,"query":"\"timed out\"","all_fields":true}},{"match_phrase":{"path":{"query":"*errInvokerResult*"}}},{"range":{"@timestamp":{"gte":1513267200000,"lte":1513353599999,"format":"epoch_millis"}}}],"must_not":[]}}}},"fragment_size":2147483647}}')
 local result = handle:read("*a")
 handle:close()
 ngx.say(result)
