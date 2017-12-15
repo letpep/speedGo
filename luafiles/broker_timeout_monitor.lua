751bec21000ee29c780332ae176dfd98f5e8df13
李信 <li.xin@sf-express.com>
2017-12-15 17:52:37 +0800

    修复3

--- luafiles/broker_timeout_monitor.lua
+++ luafiles/broker_timeout_monitor.lua
@@ -5,51 +5,21 @@
 -- Time: 下午2:57
 -- To change this template use File | Settings | File Templates.
 --
---local json = require("cjson")
---local http = require "resty.http"
---local httpc = http.new()
---local res, err = httpc:request_uri("http://10.102.4.254:5601/api/console/proxy?path=_search&method=POST", {
---    method = 'POST',
---    body = '{"version":true,"size":500,"sort":[{"@timestamp":{"order":"desc","unmapped_type":"boolean"}}],"query":{"bool":{"must":[{"query_string":{"analyze_wildcard":true,"query":"\"timed out\""}},{"match_phrase":{"path":{"query":"*errInvokerResult*"}}},{"range":{"@timestamp":{"gte":1513267200000,"lte":1513353599999,"format":"epoch_millis"}}}],"must_not":[]}},"_source":{"excludes":[]},"aggs":{"2":{"date_histogram":{"field":"@timestamp","interval":"30m","time_zone":"Asia/Shanghai","min_doc_count":1}}},"stored_fields":["*"],"script_fields":{},"docvalue_fields":["@timestamp"],"highlight":{"pre_tags":["@kibana-highlighted-field@"],"post_tags":["@/kibana-highlighted-field@"],"fields":{"*":{"highlight_query":{"bool":{"must":[{"query_string":{"analyze_wildcard":true,"query":"\"timed out\"","all_fields":true}},{"match_phrase":{"path":{"query":"*errInvokerResult*"}}},{"range":{"@timestamp":{"gte":1513267200000,"lte":1513353599999,"format":"epoch_millis"}}}],"must_not":[]}}}},"fragment_size":2147483647}}',
---    headers = {
---        ["Content-Type"] = "application/json;charset=UTF-8",
---        ["kbn-version"] = "5.6.4",
---    }
---})
---
---if not res then
---    ngx.say("failed to request: ", err)
---    return
---end
---ngx.log(ngx.ERR,json.encode(res.body))
---ngx.say(res.body)
+local json = require("cjson")
 local http = require "resty.http"
 local httpc = http.new()
-
---ngx.req.read_body()
---local postargs = ngx.req.get_post_args()
-
-local user =  ngx.var.arg_user
-local pwd =  ngx.var.arg_pwd
-
-local res, err = httpc:request_uri('http://10.102.4.252:3000/login', {
-    method = "POST",
+local res, err = httpc:request_uri("http://10.102.4.254:5601/api/console/proxy?path=_search&method=POST", {
+    method = 'POST',
+    body = '{"version":true,"size":500,"sort":[{"@timestamp":{"order":"desc","unmapped_type":"boolean"}}],"query":{"bool":{"must":[{"query_string":{"analyze_wildcard":true,"query":"timed out"}},{"match_phrase":{"path":{"query":"*errInvokerResult*"}}},{"range":{"@timestamp":{"gte":1513267200000,"lte":1513353599999,"format":"epoch_millis"}}}],"must_not":[]}},"_source":{"excludes":[]},"aggs":{"2":{"date_histogram":{"field":"@timestamp","interval":"30m","time_zone":"Asia/Shanghai","min_doc_count":1}}},"stored_fields":["*"],"script_fields":{},"docvalue_fields":["@timestamp"],"highlight":{"pre_tags":["@kibana-highlighted-field@"],"post_tags":["@/kibana-highlighted-field@"],"fields":{"*":{"highlight_query":{"bool":{"must":[{"query_string":{"analyze_wildcard":true,"query":"timed out","all_fields":true}},{"match_phrase":{"path":{"query":"*errInvokerResult*"}}},{"range":{"@timestamp":{"gte":1513267200000,"lte":1513353599999,"format":"epoch_millis"}}}],"must_not":[]}}}},"fragment_size":2147483647}}',
     headers = {
-        ["Content-Type"] = "application/json",
-    },
-    body = '{"user":"'..user..'","email":"","password":"'..pwd..'"}'
+        ["Content-Type"] = "application/json;charset=UTF-8",
+        ["kbn-version"] = "5.6.4",
+    }
 })
 
-ngx.status = res.status
-
-for k, v in pairs(res.headers) do
-    if k ~= "Transfer-Encoding" and k ~= "Connection" then
-        ngx.header[k] = v
-    end
+if not res then
+    ngx.say("failed to request: ", err)
+    return
 end
-
-if res.body == '{"message":"Logged in"}' then
-    ngx.say('ok')
-end
-
-httpc:close()
+ngx.log(ngx.ERR,json.encode(res.body))
+ngx.say(res.body)
