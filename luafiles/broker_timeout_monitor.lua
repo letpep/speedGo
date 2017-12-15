0f42546aa257ef3fdbc521bb2e6e4618a60bcc6e
李信 <li.xin@sf-express.com>
2017-12-15 18:19:24 +0800

    返回统计数量2

--- luafiles/broker_timeout_monitor.lua
+++ luafiles/broker_timeout_monitor.lua
@@ -8,9 +8,11 @@
 local json = require("cjson")
 local http = require "resty.http"
 local httpc = http.new()
+local queryjson = '"query":{"bool":{"must":[{"query_string":{"analyze_wildcard":true,"query":"\"timed out\""}},{"match_phrase":{"path":{"query":"*errInvokerResult*"}}},{"range":{"@timestamp":{"gte":1513267200000,"lte":1513353599999,"format":"epoch_millis"}}}],"must_not":[]}}';
+
 local res, err = httpc:request_uri("http://10.102.4.254:5601/api/console/proxy?path=_search&method=POST", {
     method = 'POST',
-    body = '{"version":true,"size":0,"sort":[{"@timestamp":{"order":"desc","unmapped_type":"boolean"}}],"query":{"bool":{"must":[{"query_string":{"analyze_wildcard":true,"query":"timed out"}},{"match_phrase":{"path":{"query":"*errInvokerResult*"}}},{"range":{"@timestamp":{"gte":1513267200000,"lte":1513353599999,"format":"epoch_millis"}}}],"must_not":[]}},"_source":{"excludes":[]},"aggs":{"2":{"date_histogram":{"field":"@timestamp","interval":"30m","time_zone":"Asia/Shanghai","min_doc_count":1}}},"stored_fields":["*"],"script_fields":{},"docvalue_fields":["@timestamp"],"highlight":{"pre_tags":["@kibana-highlighted-field@"],"post_tags":["@/kibana-highlighted-field@"],"fields":{"*":{"highlight_query":{"bool":{"must":[{"query_string":{"analyze_wildcard":true,"query":"timed out","all_fields":true}},{"match_phrase":{"path":{"query":"*errInvokerResult*"}}},{"range":{"@timestamp":{"gte":1513267200000,"lte":1513353599999,"format":"epoch_millis"}}}],"must_not":[]}}}},"fragment_size":2147483647}}',
+    body = '{"version":true,"size":0,'..queryjson..'}',
     headers = {
         ["Content-Type"] = "application/json;charset=UTF-8",
         ["kbn-version"] = "5.6.4",
